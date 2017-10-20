baca13d726ab29ff3ecdb0808d75ad8629f182bd
Nathan Corry <n.corry@2016.ljmu.ac.uk>
2017-10-20 16:01:57 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index)
-  if  index == ""
+  if  index == "" ||| " "
         return false
   elsif index == "X" || "O"
-
         return true
 
     end
