032ebb9a008eaec103a2b14a1871a9fa353dbf04
Nathan Corry <n.corry@2016.ljmu.ac.uk>
2017-10-20 16:18:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,position)
-  if board[position] == " "
+  if board[index] == " "
     false
     elsif board[position] == ""
     false
